de009e1ffed489d0a7da18cb93ca467df9986cc4
Nazmul Hasan Shajib <shojib116@gmail.com>
2024-07-03 15:11:07 +0600

    6.14-completed

--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,13 +1,13 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const anecdotesAtStart = [
-  "If it hurts, do it more often",
-  "Adding manpower to a late software project makes it later!",
-  "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
-  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-  "Premature optimization is the root of all evil.",
-  "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
-];
+// const anecdotesAtStart = [
+//   "If it hurts, do it more often",
+//   "Adding manpower to a late software project makes it later!",
+//   "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+//   "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+//   "Premature optimization is the root of all evil.",
+//   "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+// ];
 
 const getId = () => (100000 * Math.random()).toFixed(0);
 
@@ -46,7 +46,7 @@ const asObject = (anecdote) => {
 
 const anecdoteSlice = createSlice({
   name: "anecdotes",
-  initialState: anecdotesAtStart.map(asObject),
+  initialState: [],
   reducers: {
     createAnecdote(state, action) {
       return [...state, asObject(action.payload)];
@@ -62,6 +62,9 @@ const anecdoteSlice = createSlice({
       return state.map((anecdote) =>
         anecdote.id === id ? updatedAnecdote : anecdote
       );
+    },
+    setAnecdotes(state, action) {
+      return action.payload;
     },
   },
 });
@@ -80,5 +83,6 @@ const anecdoteSlice = createSlice({
 //   };
 // };
 
-export const { createAnecdote, updateVotes } = anecdoteSlice.actions;
+export const { createAnecdote, updateVotes, setAnecdotes } =
+  anecdoteSlice.actions;
 export default anecdoteSlice.reducer;
